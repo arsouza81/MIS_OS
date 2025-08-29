c16b0375c413148de4a6568726f5e5a26e08c08c
Anderson Souza <153129699+arsouza81@users.noreply.github.com>
2025-08-29 17:23:10 -0400

    feat: versão inicial do sistema

---
+++ codigo_fonte/OrdemDeServico/Migrations/20241002213328_Database create.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrdemDeServico.Migrations
+{
+    /// <inheritdoc />
+    public partial class Databasecreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
