68e0ae545333607ab6427984fed39a595df75ef5
Rennan Alves <rennandev7@gmail.com>
2025-09-30 14:25:56 -0400

    chore: Arquivos PaginaController.cs/Views.cs e pasta wwwroot apagados, conteúdos relacionados a frontend removidos da pasta backend

--- codigo_fonte/backend/OrdemDeServico/Controllers/FormServidorController.cs
+++ codigo_fonte/backend/OrdemDeServico/Controllers/FormServidorController.cs
@@ -1,4 +1,4 @@
-﻿using AutoMapper;
+﻿﻿using AutoMapper;
 using HandlebarsDotNet;
 using Microsoft.AspNetCore.Mvc;
 using OrdemDeServico.Data;
--- codigo_fonte/backend/OrdemDeServico/Controllers/UserController.cs
+++ codigo_fonte/backend/OrdemDeServico/Controllers/UserController.cs
@@ -1,4 +1,4 @@
-﻿using AutoMapper;
+﻿﻿using AutoMapper;
 using HandlebarsDotNet;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication;
